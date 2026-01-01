85d4f2483c2b51ca8357808bd8b5aa6b3228a84a
Ege Aydın <151860320+egegeegege@users.noreply.github.com>
2026-01-01 18:25:34 +0300

    .

--- src/components/TemplatePreview.tsx
+++ src/components/TemplatePreview.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 interface TemplatePreviewProps {
   templateId: string;
